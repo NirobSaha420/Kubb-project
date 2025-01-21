6679200ffe9de93a7f9683eb61e1b1790204a108
NirobSaha420 <anight908@gmail.com>
2025-01-21 22:43:25 +0600

    project added

---
+++ JS/custom.js
@@ -0,0 +1,112 @@
+// Add active class to the current button (highlight it)
+var header = document.getElementById("nav-parent");
+var btns = header.getElementsByClassName("nav-link");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}
+
+// Slider start
+$('.banner-slider').slick({
+  dots: false,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+
+$('.common-slider').slick({
+  dots: true,
+  arrows: true,
+  infinite: true,
+  speed: 600,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.portfolio-slider').slick({
+  arrows: false,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  delay: 1000,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+// slider end
+
+
+
+// dark mode
+
+function dark() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+
+// dark mode
